9a34625740d48332d7c4f5e8d5a043569fde4bf9
Adam Smith <adamsmith910@aol.com>
2014-11-03 19:32:19 -0700

    order glenarm denver to erb

--- backcountry_deli_app.rb
+++ backcountry_deli_app.rb
@@ -16,6 +16,12 @@ class BackCountryApp < Sinatra::Base
                             body_class:  "page page-id-6 page-template-default"}
   end
 
+  get '/order_glenarm_denver' do 
+    erb :order_glenarm_denver, locals:{page_name:  :order_glenarm_denver,
+                                       title:  "Order Online - Denver 17th & Glenarm - Backcountry Delicatesse",
+                                       body_class:  "page page-id-643 page-template page-template-page-home-php"}
+  end
+
   get '/customer_policies' do
     erb :customer_policies, locals:{page_name:  :customer_policies,
                                     title:  "Backcountry Policies - Backcountry Delicatessen",
@@ -49,7 +55,9 @@ class BackCountryApp < Sinatra::Base
   end
 
   get '/hot_breakfast_sandwiches' do
-    erb :hot_breakfast_sandwiches
+    erb :hot_breakfast_sandwiches, locals:{page_name: :hot_breakfast_sandwiches,
+                                           title:  "Hot Breakfast Sandwiches - Backcountry Delicatessen",
+                                           body_class:  "page page-id-51 page-child parent-pageid-9 page-template page-template-page-menu-php"}
   end
 
   get '/build_your_own' do
