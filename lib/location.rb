fa8c046ee75082d245ceeac9c07800e99ba6ee88
Viki Harrod <victoria.harrod@gmail.com>
2014-11-05 15:02:12 -0700

    add location_store and location classes, make footer dynamically related to both

--- lib/location.rb
+++ lib/location.rb
@@ -1,12 +1,15 @@
 class Location
-  attr_reader :name, :hours1, :hours2, :area, :address, :city_state_zip, :phone
+  attr_reader :id, :name, :hours1, :hours2, :area, :address, :city_state_zip, :phone, :twitter_link
+
   def initialize(data)
+    @id = data[:id]
     @name = data[:name]
     @hours1 = data[:hours1]
     @hours2 = data[:hours2]
     @area = data[:area]
     @address = data[:address]
     @city_state_zip = data[:city_state_zip]
-    @phone = data[:phone]
+    @phone = data[:phone_number]
+    @twitter_link = data[:twitter_link]
   end
 end
